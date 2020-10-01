4186a52387a53f0946fcc1372d4f52850c11eabf
pham quang <kpfphamquang@gmail.com>
2020-10-01 11:03:40 +0700

    quang

--- admin-dashboard/src/app/cores/guards/auth.guard.ts
+++ admin-dashboard/src/app/cores/guards/auth.guard.ts
@@ -1,23 +1,23 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-// import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthGuard implements CanActivate {
-//   constructor(private router: Router){}
-//   canActivate(
-//     next: ActivatedRouteSnapshot,
-//     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-//       const accessToken = localStorage.getItem('access_token');
-//       if(accessToken){
-//         this.router.navigate(['/main']);
-//         return true;
-//       } else {
-//         this.router.navigate(['/login']);
-//         return false ;  
-//       }
-//   }
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router){}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const accessToken = localStorage.getItem('access_token');
+      if(accessToken){
+        this.router.navigate(['/main']);
+        return true;
+      } else {
+        this.router.navigate(['/login']);
+        return false ;  
+      }
+  }
   
-// }
+}
--- admin-dashboard/src/app/cores/helpers/http-error.interceptor.ts
+++ admin-dashboard/src/app/cores/helpers/http-error.interceptor.ts
@@ -1,37 +1,36 @@
-// import { Injectable } from '@angular/core';
-// import {
-//   HttpRequest,
-//   HttpHandler,
-//   HttpEvent,
-//   HttpInterceptor, HttpErrorResponse
-// } from '@angular/common/http';
-// import { Observable, throwError } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
-// @Injectable()
-// export class HttpErrorInterceptor implements HttpInterceptor {
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
 
-//   constructor() {}
+  constructor() {}
 
-//   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-//     return next.handle(request)
-//     .pipe(
-//       catchError((err: HttpErrorResponse) => {
-//         if(err.status === 401){
-//           console.log("Your session is expired")
-//           setTimeout(() => { window.location.href = '/account/login' }, 3000);
-//             return;
-//         }
-//         const error = err.error;
-//         let errMsg = '';
-//         if(error instanceof ErrorEvent ){
-//           errMsg = `Eroor : ${error.message}`
-//         } else {
-//           errMsg = error ? `${error.message || err.message}` : err.message;
-//         }
-//         return throwError(errMsg);
-//       })
-//     )
-//   }
-// }
-  
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request)
+    .pipe(
+      catchError((err: HttpErrorResponse) => {
+        if(err.status === 401){
+          console.log("Your session is expired")
+          setTimeout(() => { window.location.href = '/account/login' }, 3000);
+            return;
+        }
+        const error = err.error;
+        let errMsg = '';
+        if(error instanceof ErrorEvent ){
+          errMsg = `Eroor : ${error.message}`
+        } else {
+          errMsg = error ? `${error.message || err.message}` : err.message;
+        }
+        return throwError(errMsg);
+      })
+    )
+  }
+}
